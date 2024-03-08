c92ce0a8be6997553ad5ab972fe2279d6b0480ff
yuki <yu.xie@wiz.ai>
2024-03-08 14:49:34 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ export CONTAINER_NAME=$(jq -r '.[0].name' config.json) >> $LOG_FILE 2>&1
 export IMAGE_URI=$(jq -r '.[0].imageUri' config.json) >> $LOG_FILE 2>&1
 
 # 登录到Amazon ECR
-aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${ECR_REPOSITORY_URI} >> $LOG_FILE 2>&1
+# aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${ECR_REPOSITORY_URI} >> $LOG_FILE 2>&1
 
 echo "容器名称: $CONTAINER_NAME"
 echo "镜像URI: $IMAGE_URI"
