55994c666f5002dceffc3c893fbec549f84054da
yuki <yu.xie@wiz.ai>
2024-03-08 14:16:28 +0800

    chore: deploy增加日志打印

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+
 set -e
 
-export AWS_DEFAULT_REGION=$(jq -r '.[0].region' config.json)
-export ECR_REPOSITORY_URI=$(jq -r '.[0].repositoryUri' config.json)
-export CONTAINER_NAME=$(jq -r '.[0].name' config.json)
-export IMAGE_URI=$(jq -r '.[0].imageUri' config.json)
+LOG_FILE="/var/log/deploy.log"
+
+export AWS_DEFAULT_REGION=$(jq -r '.[0].region' config.json) >> $LOG_FILE 2>&1
+export ECR_REPOSITORY_URI=$(jq -r '.[0].repositoryUri' config.json) >> $LOG_FILE 2>&1
+export CONTAINER_NAME=$(jq -r '.[0].name' config.json) >> $LOG_FILE 2>&1
+export IMAGE_URI=$(jq -r '.[0].imageUri' config.json) >> $LOG_FILE 2>&1
 
 # 登录到Amazon ECR
-aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${ECR_REPOSITORY_URI}
+aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${ECR_REPOSITORY_URI} >> $LOG_FILE 2>&1
 
 echo "容器名称: $CONTAINER_NAME"
 echo "镜像URI: $IMAGE_URI"
 
 # 拉取最新的镜像
-docker pull ${IMAGE_URI}
+docker pull ${IMAGE_URI} >> $LOG_FILE 2>&1
 
 # 使用docker-compose重启服务
-docker-compose down && docker-compose up -d
+docker-compose down && docker-compose up -d >> $LOG_FILE 2>&1
