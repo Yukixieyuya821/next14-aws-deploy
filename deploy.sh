5d2d11065e548fb96096500c28503b0c4cfb5ad2
yuki <yu.xie@wiz.ai>
2024-03-08 11:31:31 +0800

    chore: 调整部署配置

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
+export AWS_DEFAULT_REGION=$(jq -r '.[0].region' config.json)
+export ECR_REPOSITORY_URI=$(jq -r '.[0].repositoryUri' config.json)
+export CONTAINER_NAME=$(jq -r '.[0].name' config.json)
+export IMAGE_URI=$(jq -r '.[0].imageUri' config.json)
+
 # 登录到Amazon ECR
 aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${ECR_REPOSITORY_URI}
-
-export CONTAINER_NAME=$(jq -r '.[0].name' imagedefinitions.json)
-export IMAGE_URI=$(jq -r '.[0].imageUri' imagedefinitions.json)
 
 echo "容器名称: $CONTAINER_NAME"
 echo "镜像URI: $IMAGE_URI"
