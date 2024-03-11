c7325a4933e623f7ae24ab23306b436bfe041742
yuki <yu.xie@wiz.ai>
2024-03-11 11:44:06 +0800

    feat: upgrade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <b>Hello Yuki</b>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
